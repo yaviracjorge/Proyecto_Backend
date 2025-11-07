935ac804a0b0796547fda13c4f8160d7907eb30a
angel0314ist <alc.quishpe@yavirac.edu.ec>
2025-11-07 18:43:28 -0500

    investigar

--- src/main/java/com/proyecto/backend/app/backend_app/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/proyecto/backend/app/backend_app/auth/filter/JwtAuthenticationFilter.java
@@ -1,10 +1,16 @@
 package com.proyecto.backend.app.backend_app.auth.filter;
 
+import java.io.IOException;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.proyecto.backend.app.backend_app.model.entities.Usuario;
 
 
@@ -17,10 +23,18 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
-        return authenticationManager.authenticate(null);    
-            
-    //  Usuario usuario = null;
-    //  String correo = null;
-    //  String contrasena = null;
+       try{
+        Usuario usuario = null;
+        String correo = null;
+        String contrasena = null;
+        usuario = new ObjectMapper().readValue(request.getInputStream(),Usuario.class);
+       } catch(StreamReadException e){
+        e.printStackTrace();
+       } catch (DatabindException e){
+        e.printStackTrace();
+       } catch (IOException e){
+        e.printStackTrace();
+       } 
+        
     }
 }
